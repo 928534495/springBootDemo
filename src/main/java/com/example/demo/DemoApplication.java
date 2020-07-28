c54438dc1a7857561835cb8f9a6c21af17621d58
928534495 <928534495@qq.com>
2020-07-28 15:02:00 +0800

    初次分享

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class DemoApplication {
-
+//启动器
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
