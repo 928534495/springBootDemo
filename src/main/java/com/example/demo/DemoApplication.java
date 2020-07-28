bc8d7a7d942d0b25ef2ad3e5907078051aac3f43
wangyaowu1994 <584030001@qq.com>
2020-07-28 17:04:31 +0800

    初次分享

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class DemoApplication {
+    //王耀武修改
 //启动器
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
